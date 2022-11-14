a5adf462edb972d3ecf260b87fbfcf21e87092ba
mayankchadha16 <chadhamayank1609@gmail.com>
2022-11-14 15:53:57 +0530

    add optimized code

--- factorial.py
+++ factorial.py
@@ -2,11 +2,4 @@
 # using recursion
 
 def factorial(x):
-    """This is a recursive function
-    to find the factorial of an integer"""
-
-    if x == 0:
-        return 1
-    else:
-        # recursive call to the function
-        return (x * factorial(x-1))
+    return 1 if x == 0 else (x * factorial(x-1))
